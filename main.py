03b6c23b73969b8c7607b308c7bc6e4f4e4ba3ca
Andrey <141425370+n1ke03@users.noreply.github.com>
2024-04-05 19:13:17 +0300

    2 task

--- main.py
+++ main.py
@@ -8,14 +8,37 @@
 # Если пользователь ввел значение не в диапазоне от 1
 # до 100 требуется вывести сообщение об ошибке.
 
-num = int(input("Введите целое число от 1 до 100\n"))
-if num % 3 == 0 and num % 5 == 0:
-    print("Fizz Buzz")
-elif num % 5 == 0:
-    print("Buzz")
-elif num % 3 == 0:
-    print("Fizz")
-elif num % 3 and num % 5:
-    print(num)
-else:
-    print("Не верный ввод")
+# num = int(input("Введите целое число от 1 до 100\n"))
+# if num % 3 == 0 and num % 5 == 0:
+#     print("Fizz Buzz")
+# elif num % 5 == 0:
+#     print("Buzz")
+# elif num % 3 == 0:
+#     print("Fizz")
+# elif num % 3 and num % 5:
+#     print(num)
+# else:
+#     print("Не верный ввод")
+
+
+
+# num_1 = int(input("Введите целое число: "))
+# num_2 = int(input("Введите в какую степень возвести от 0 до 7: "))
+# if num_2 == 0:
+#     print(num_1 ** num_2)
+# elif num_2 == 1:
+#     print(num_1 ** num_2)
+# elif num_2 == 2:
+#     print(num_1 ** num_2)
+# elif num_2 == 3:
+#     print(num_1 ** num_2)
+# elif num_2 == 4:
+#     print(num_1 ** num_2)
+# elif num_2 == 5:
+#     print(num_1 ** num_2)
+# elif num_2 == 6:
+#     print(num_1 ** num_2)
+# elif num_2 == 7:
+#     print(num_1 ** num_2)
+# else:
+#     print("Не верный ввод")
